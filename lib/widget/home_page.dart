529463b5b193c5a6434f69b54242bbe5dacc0f45
Aleksey Kuzmin <kboomska@gmail.com>
2024-07-12 21:20:39 +0300

    Добавляем немного красок для удобства отслеживания перерисовки виджетов

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 /// Оригинальный пример счетчика, который генерируется при создании нового
@@ -5,9 +7,7 @@ import 'package:flutter/material.dart';
 ///
 /// При увеличении значения счетчика перерисовывается буквально весь экран.
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -24,23 +24,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // Генерация случайного цвета Scaffold для контроля его перерисовки.
+    final scaffoldBodyColor = Colors.primaries[Random().nextInt(
+      Colors.primaries.length,
+    )];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('StatefulWidget and setState()'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: scaffoldBodyColor,
+            width: 8,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextWidget(),
+              CounterWidget(counter: _counter),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -48,6 +56,52 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+/// Виджет текста.
+///
+/// Конструктор виджета нарочно сделан не константным, чтобы виджет мог
+/// перерисовываться при обновлении родительского виджета.
+class TextWidget extends StatelessWidget {
+  TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Генерация случайного цвета текста для контроля его перерисовки.
+    final textColor = Colors.primaries[Random().nextInt(
+      Colors.primaries.length,
+    )];
+
+    return Text(
+      'You have pushed the button this many times:',
+      style: TextStyle(color: textColor),
+    );
+  }
+}
+
+/// Виджет счетчика.
+class CounterWidget extends StatelessWidget {
+  const CounterWidget({
+    super.key,
+    required int counter,
+  }) : _counter = counter;
+
+  final int _counter;
+
+  @override
+  Widget build(BuildContext context) {
+    // Генерация случайного цвета счетчика для контроля его перерисовки.
+    final counterColor = Colors.primaries[Random().nextInt(
+      Colors.primaries.length,
+    )];
+
+    return Text(
+      '$_counter',
+      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+            color: counterColor,
+          ),
     );
   }
 }
